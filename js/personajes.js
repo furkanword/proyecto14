b1db343267d69160dfb0c3c7bc2df7a2cb9403aa
Kevin Arce <arcekevin898@gmail.com>
2023-03-16 09:26:23 -0500

    proceso3

---
+++ js/personajes.js
@@ -0,0 +1,11 @@
+personajeCompleto = document.querySelector(`#nombre, #poder`);
+
+addEventListener(onclick) = document.getElementById('blok2');
+
+function mostrar(personajeCompleto) {
+    
+
+    
+}  
+
+
